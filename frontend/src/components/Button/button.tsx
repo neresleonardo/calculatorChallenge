02d1d423d6ea9230b81aa56da864ff4618752c30
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-02-17 13:11:13 -0300

    init components

---
+++ frontend/src/components/Button/button.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Container} from './styles'
+
+interface buttonProps {
+    label: string,
+    operation?: boolean,
+    double?: boolean,
+           
+}
+
+export function Button({label , double, operation}:buttonProps) {
+    return (
+        <Container>
+            <button 
+                className={
+                `button
+                ${operation ? 'operation' : ''}
+                ${double ? 'double' : ''}
+                `
+            } >{label}</button>
+        </Container>
+    )
+}
